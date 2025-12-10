991e3b463f124a6235a056a8002e99e86afaca9e
javirg11 <javirg10@gmail.com>
2025-12-10 15:44:12 +0100

    feat: new component titles

--- frontend/src/app/pages/formation/formation.component.ts
+++ frontend/src/app/pages/formation/formation.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-formation',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './formation.component.html',
   styleUrl: './formation.component.scss'
 })
--- frontend/src/app/titles/titles.component.ts
+++ frontend/src/app/titles/titles.component.ts
@@ -1,11 +1,32 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-titles',
-  imports: [],
+  imports: [NgFor, NgIf],
   templateUrl: './titles.component.html',
   styleUrl: './titles.component.scss'
 })
 export class TitlesComponent {
 
+titulos = [
+    {
+      nombre: 'Título Grado en Ing. Sis. de Información',
+      imagen: '/assets/tittles/certificado_grado.jpg'
+    },
+    {
+      nombre: 'Título Grado Sup. Sistemas de Telec. e Informáticos',
+      imagen: '/assets/tittles/tituloFP.jpg'
+    }
+  ];
+
+  imagenSeleccionada: string | null = null;
+
+  abrirImagen(imagen: string) {
+    this.imagenSeleccionada = imagen;
+  }
+
+  cerrarImagen() {
+    this.imagenSeleccionada = null;
+  }
 }
